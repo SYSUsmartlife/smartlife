3003e28e305075ba0f4bd4d6e7b683b93e0ca8ed
kuxinwei <kuxinwei@15d0d55b-4310-4b79-9731-e7fb742fba7c>
2014-06-12 12:04:36 +0000

    添加/bin到ignore列表，添加gen文件夹到版本控制
    git-svn-id: http://www.svnchina.com/svn/smartlife@3 15d0d55b-4310-4b79-9731-e7fb742fba7c

--- SmartLife/src/com/smartlife/activity/MainActivity.java
+++ SmartLife/src/com/smartlife/activity/MainActivity.java
@@ -1,7 +1,6 @@
 package com.smartlife.activity;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,7 +8,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 
 public class MainActivity extends Activity {
 
@@ -27,7 +25,6 @@ public class MainActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
