f52dbf9811020a08cd83a75f44de37bd1dc993ac
IceCreamWu <IceCreamWu@15d0d55b-4310-4b79-9731-e7fb742fba7c>
2014-07-06 07:53:26 +0000

    新增登录和注册界面，具体功能还未实现
    git-svn-id: http://www.svnchina.com/svn/smartlife@6 15d0d55b-4310-4b79-9731-e7fb742fba7c

---
+++ SmartLife/src/com/smartlife/activity/LoginActivity.java
@@ -0,0 +1,80 @@
+/**
+ * @author 吴湧霖
+ * @version 创建时间：2014年7月6日 上午12:09:26 
+ */
+package com.smartlife.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * 应用程序登录界面
+ */
+public class LoginActivity extends Activity implements OnClickListener{
+
+	/**
+	 * 登录按钮
+	 */
+	private Button loginButton;
+	/**
+	 * 跳转到注册界面的链接
+	 */
+	private TextView registerLinkText;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_login);
+		
+		initView();
+	}
+
+	/**
+	 * 初始化各控件
+	 */
+	private void initView() {
+		loginButton = (Button)findViewById(R.id.btn_login);
+		loginButton.setOnClickListener(this);
+		registerLinkText = (TextView)findViewById(R.id.text_reg_link);
+		registerLinkText.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.btn_login:
+			login();
+			break;
+		case R.id.text_reg_link:
+			toRegister();
+			break;
+		default:
+			break;
+		}
+	}
+
+	/**
+	 * 登录操作
+	 */
+	private void login() {
+		Intent intent = new Intent(this, HomeActivity.class);
+		startActivity(intent);
+	}
+
+	/**
+	 * 跳转到注册界面
+	 */
+	private void toRegister() {
+		Intent intent = new Intent(this, RegisterActivity.class);
+		startActivity(intent);
+		this.finish();
+	}
+
+}
---
+++ SmartLife/src/com/smartlife/activity/RegisterActivity.java
@@ -0,0 +1,76 @@
+/**
+ * @author 吴湧霖
+ * @version 创建时间：2014年7月6日 下午3:24:01 
+ */
+package com.smartlife.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class RegisterActivity extends Activity implements OnClickListener{
+
+	/**
+	 * 注册按钮
+	 */
+	private Button registerButton;
+	/**
+	 * 跳转到登录界面的链接
+	 */
+	private TextView loginLinkText;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_register);
+		
+		initView();
+	}
+
+	/**
+	 * 初始化各控件
+	 */
+	private void initView() {
+		registerButton = (Button)findViewById(R.id.btn_register);
+		registerButton.setOnClickListener(this);
+		loginLinkText = (TextView)findViewById(R.id.text_login_link);
+		loginLinkText.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.btn_register:
+			register();
+			break;
+		case R.id.text_login_link:
+			toLogin();
+			break;
+		default:
+			break;
+		}
+	}
+
+	/**
+	 * 注册操作
+	 */
+	private void register() {
+		
+	}
+
+	/**
+	 * 跳转到登录界面
+	 */
+	private void toLogin() {
+		Intent intent = new Intent(this, LoginActivity.class);
+		startActivity(intent);
+		this.finish();
+	}
+
+}
