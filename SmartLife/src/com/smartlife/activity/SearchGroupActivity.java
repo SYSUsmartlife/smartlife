f96019308385b3294f24907841e56722f0cdf634
IceCreamWu <286407114@qq.com>
2014-07-11 00:20:16 +0800

    添加查找群组的布局文件

--- SmartLife/src/com/smartlife/activity/SearchGroupActivity.java
+++ SmartLife/src/com/smartlife/activity/SearchGroupActivity.java
@@ -14,7 +14,7 @@ public class SearchGroupActivity extends Activity{
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		setContentView(R.layout.activity_create_group);
+		setContentView(R.layout.activity_search_group);
 	}
 	
 }
