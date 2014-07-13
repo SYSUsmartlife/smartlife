1cf6a165e9ba5860c1afd19b875d616c331f49fa
kuxinwei <kuxinwei@163.com>
2014-07-13 21:39:08 +0800

    完成添加创建活动界面；

--- SmartLife/src/com/smartlife/activity/TaskActivity.java
+++ SmartLife/src/com/smartlife/activity/TaskActivity.java
@@ -8,15 +8,18 @@ package com.smartlife.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.view.Window;
 
+import com.smartlife.fragment.CreateTaskFragment;
 import com.smartlife.fragment.DetailTaskFragment;
 import com.smartlife.model.Task;
 
 public class TaskActivity extends FragmentActivity {
 
 	Task task;
+	Fragment fragment;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -27,13 +30,15 @@ public class TaskActivity extends FragmentActivity {
 
 		Intent intent = getIntent();
 		Bundle bundle = intent.getExtras();
-		if (bundle.getSerializable(Task.TAG) instanceof Task)
+		if (bundle != null && bundle.getSerializable(Task.TAG) instanceof Task)
 			task = (Task) bundle.get(Task.TAG);
 
 		if (task != null)
-			getSupportFragmentManager()
-					.beginTransaction()
-					.replace(R.id.task_container,
-							DetailTaskFragment.newInstance(task)).commit();
+			fragment = DetailTaskFragment.newInstance(task);
+		else
+			fragment = new CreateTaskFragment();
+
+		getSupportFragmentManager().beginTransaction()
+				.replace(R.id.task_container, fragment).commit();
 	}
 }
